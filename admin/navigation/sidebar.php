94aa97a785446857485a3967a727ac52b5bf1b91
iluvmaisan <pierrecarl.tolentino.cics@ust.edu.ph>
2025-01-23 22:51:25 +0800

    updated the icons

--- admin/navigation/sidebar.php
+++ admin/navigation/sidebar.php
@@ -3,43 +3,43 @@
     <ul>
         <li>
             <a href="dashboard.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Dashboard Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Dashboard Icon" class="nav-icon">
                 <span>Dashboard</span>
             </a>
         </li>
         <li>
             <a href="parts.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Parts Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Parts Icon" class="nav-icon">
                 <span>Parts</span>
             </a>
         </li>
         <li>
             <a href="transactions.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Transactions Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Transactions Icon" class="nav-icon">
                 <span>Transactions</span>
             </a>
         </li>
         <li>
             <a href="logs.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Logs Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Logs Icon" class="nav-icon">
                 <span>Logs</span>
             </a>
         </li>
         <li>
             <a href="users.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Users Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Users Icon" class="nav-icon">
                 <span>Users</span>
             </a>
         </li>
         <li>
             <a href="suppliers.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Suppliers Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Suppliers Icon" class="nav-icon">
                 <span>Suppliers</span>
             </a>
         </li>
         <li>
             <a href="services.php">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Services Icon" class="nav-icon">
+                <img src="/images/sideicon.png" alt="Services Icon" class="nav-icon">
                 <span>Services</span>
             </a>
         </li>
