486d79ca4d27cad1e8a3562e85fb95eb419dca63
iluvmaisan <pierrecarl.tolentino.cics@ust.edu.ph>
2025-01-23 22:58:43 +0800

    updated the icons staff

--- staff/navigation/sidebar.php
+++ staff/navigation/sidebar.php
@@ -2,22 +2,22 @@
     <ul class="nav-list">
         <li>
             <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Dashboard Icon" class="nav-icon"> Home
+                <img src="/images/sideicon.png" alt="Dashboard Icon" class="nav-icon"> Home
             </a>
         </li>
         <li>
             <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Dashboard Icon" class="nav-icon"> Parts
+                <img src="/images/sideicon.png" alt="Dashboard Icon" class="nav-icon"> Parts
             </a>
         </li>
         <li>
             <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Dashboard Icon" class="nav-icon"> Services
+                <img src="/images/sideicon.png" alt="Dashboard Icon" class="nav-icon"> Services
             </a>
         </li>
         <li>
             <a href="#">
-                <img src="https://cdn-icons-png.flaticon.com/512/10218/10218090.png" alt="Dashboard Icon" class="nav-icon"> Suppliers
+                <img src="/images/sideicon.png" alt="Dashboard Icon" class="nav-icon"> Suppliers
             </a>
         </li>
     </ul>
