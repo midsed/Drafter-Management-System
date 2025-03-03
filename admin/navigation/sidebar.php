4ab48e206ce660515159b33d5acd14f9cd463c94
iluvmaisan <pierrecarl.tolentino.cics@ust.edu.ph>
2025-03-03 17:19:42 +0800

    updated sidebar icons, admin and staff

--- admin/navigation/sidebar.php
+++ admin/navigation/sidebar.php
@@ -13,37 +13,37 @@ $current_page = basename($_SERVER['PHP_SELF']);
         </li>
         <li class="<?php echo ($current_page == 'parts.php') ? 'active' : ''; ?>">
             <a href="parts.php">
-                <img src="images/sideicon.png" alt="Parts Icon" class="nav-icon">
+                <img src="images/parts1.png" alt="Parts Icon" class="nav-icon">
                 <span>Parts</span>
             </a>
         </li>
         <li class="<?php echo ($current_page == 'receipts.php') ? 'active' : ''; ?>">
             <a href="receipts.php">
-                <img src="images/sideicon.png" alt="Transactions Icon" class="nav-icon">
+                <img src="images/receipts1.png" alt="Transactions Icon" class="nav-icon">
                 <span>Receipts</span>
             </a>
         </li>
         <li class="<?php echo ($current_page == 'logs.php') ? 'active' : ''; ?>">
             <a href="logs.php">
-                <img src="images/sideicon.png" alt="Logs Icon" class="nav-icon">
+                <img src="images/results.png" alt="Logs Icon" class="nav-icon">
                 <span>Logs</span>
             </a>
         </li>
         <li class="<?php echo ($current_page == 'users.php') ? 'active' : ''; ?>">
             <a href="users.php">
-                <img src="images/sideicon.png" alt="Users Icon" class="nav-icon">
+                <img src="images/users.png" alt="Users Icon" class="nav-icon">
                 <span>Users</span>
             </a>
         </li>
         <li class="<?php echo ($current_page == 'supplier.php') ? 'active' : ''; ?>">
             <a href="supplier.php">
-                <img src="images/sideicon.png" alt="Suppliers Icon" class="nav-icon">
+                <img src="images/supplier.png" alt="Suppliers Icon" class="nav-icon">
                 <span>Suppliers</span>
             </a>
         </li>
         <li class="<?php echo ($current_page == 'service.php') ? 'active' : ''; ?>">
             <a href="service.php">
-                <img src="images/sideicon.png" alt="Services Icon" class="nav-icon">
+                <img src="images/service.png" alt="Services Icon" class="nav-icon">
                 <span>Services</span>
             </a>
         </li>
--- staff/navigation/sidebar.php
+++ staff/navigation/sidebar.php
@@ -12,19 +12,19 @@ $current_page = basename($_SERVER['PHP_SELF']);
         </li>
         <li class="<?php echo ($current_page == 'parts.php') ? 'active' : ''; ?>">
             <a href="parts.php">
-                <img src="images/sideicon.png" alt="Parts Icon" class="nav-icon">
+                <img src="images/parts1.png" alt="Parts Icon" class="nav-icon">
                 <span>Parts</span> 
             </a>
         </li>
         <li class="<?php echo ($current_page == 'service.php') ? 'active' : ''; ?>">
             <a href="service.php">
-                <img src="images/sideicon.png" alt="Services Icon" class="nav-icon">
+                <img src="images/service.png" alt="Services Icon" class="nav-icon">
                 <span>Services</span> 
             </a>
         </li>
         <li class="<?php echo ($current_page == 'supplier.php') ? 'active' : ''; ?>">
             <a href="supplier.php">
-                <img src="images/sideicon.png" alt="Suppliers Icon" class="nav-icon">
+                <img src="images/supplier.png" alt="Suppliers Icon" class="nav-icon">
                 <span>Suppliers</span> 
             </a>
         </li>
