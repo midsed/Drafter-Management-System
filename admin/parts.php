17ed98921a61eabc318a15330ff803eaff89ee29
iluvmaisan <pierrecarl.tolentino.cics@ust.edu.ph>
2025-03-22 00:21:11 +0800

    revert color qty

--- admin/parts.php
+++ admin/parts.php
@@ -1,11 +1,8 @@
 <?php 
 session_start();
 include('dbconnect.php');
-if (isset($_SESSION['UserID']) && $_SESSION['RoleType'] != 'Admin') {
-    echo "<script>
-            alert('Unauthorized access.');
-            window.location.href = '/Drafter-Management-System/login.php';
-          </script>";
+if (!isset($_SESSION['UserID'])) {
+    header("Location: /Drafter-Management-System/login.php");
     exit();
 }
 include('navigation/sidebar.php');
@@ -133,7 +130,7 @@ include('navigation/topbar.php');
                     <div class='actions'>
                         <button class='qty-btn' onclick='decreaseQuantity({$part['PartID']})'>-</button>
                         <input type='text' id='quantity_{$part['PartID']}' value='{$part['Quantity']}'" . ($isOutOfStock ? " class='quantity-input zero-quantity'" : " class='quantity-input'") . " readonly>
-                        <button class='qty-bt1' onclick='increaseQuantity({$part['PartID']})'>+</button>
+                        <button class='qty-btn' onclick='increaseQuantity({$part['PartID']})'>+</button>
                     </div>
                     <div class='actions card-actions'>
                         <a href='partsedit.php?id={$part['PartID']}' class='red-button edit-btn'>Edit</a>
@@ -736,15 +733,6 @@ body {
 }
 .qty-btn {
     background-color: #d8dcde;
-    border: 1px;
-    border-radius: 5px;
-    padding: 5px 20px;
-    cursor: pointer;
-    font-family: 'Poppins', sans-serif;
-    font-weight: 900;
-}
-.qty-bt1{
-    background-color: #32CD32;
     border: 1px;
     border-radius: 5px;
     padding: 5px 20px;
