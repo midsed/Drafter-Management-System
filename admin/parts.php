8ec6910de422c529a93a5cb34355dec66f7a8ca2
iluvmaisan <pierrecarl.tolentino.cics@ust.edu.ph>
2025-03-21 14:23:22 +0800

    updated parts

--- admin/parts.php
+++ admin/parts.php
@@ -130,7 +130,7 @@ include('navigation/topbar.php');
                     <div class='actions'>
                         <button class='qty-btn' onclick='decreaseQuantity({$part['PartID']})'>-</button>
                         <input type='text' id='quantity_{$part['PartID']}' value='{$part['Quantity']}'" . ($isOutOfStock ? " class='quantity-input zero-quantity'" : " class='quantity-input'") . " readonly>
-                        <button class='qty-btn' onclick='increaseQuantity({$part['PartID']})'>+</button>
+                        <button class='qty-bt1' onclick='increaseQuantity({$part['PartID']})'>+</button>
                     </div>
                     <div class='actions card-actions'>
                         <a href='partsedit.php?id={$part['PartID']}' class='red-button edit-btn'>Edit</a>
@@ -730,11 +730,22 @@ body {
 }
 .part-card .actions {
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
     margin-top: 10px;
 }
 .qty-btn {
     background-color: #d8dcde;
+    border: 1px;
+    border-radius: 5px;
+    padding: 5px 20px;
+    cursor: pointer;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 900;
+}
+.qty-bt1{
+    background-color: #32CD32;
     border: 1px;
     border-radius: 5px;
     padding: 5px 20px;
@@ -893,9 +904,10 @@ body {
 }
 
 .selected-card {
-    border: 6px solid #E10F0F;
+    border: 6px solid rgba(225, 15, 15, 0.7);
     animation: pulse 0.5s ease-out;
 }
+
 @keyframes pulse {
     0% {
         transform: scale(1);
@@ -910,6 +922,7 @@ body {
         box-shadow: 0 0 0 0 rgba(255, 0, 0, 0);
     }
 }
+
 
 .part-card .card-actions {
     display: flex;
